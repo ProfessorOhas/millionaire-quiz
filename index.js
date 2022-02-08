b0bc35abbbb739d2ac52d5ba3a42b08cb60f95ca
OJ <ojaschaturvedi2006@gmail.com>
2022-02-08 15:33:28 -0700

    Importing from modules

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
+import gradient from 'gradient-string';
+import inquirer from 'inquirer';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+        choices: [
+            'Peacock',
+            'Markhor', //correct
+            'Lion',
+        ],
+    });
+    return handleAnswer(answer.animal_question == 'Markhor');
+}
+
+async function food_question() {
+    const answer = await inquirer.prompt({
+        name: 'food_question',
+        type: 'list',
+        message: 'A la Crecy is a French dish made of what?',
+        choices: [
+            'Apples',
+            'Carrots', //correct
+            'Potatoes',
+        ],
+    });
+    return handleAnswer(answer.food_question == 'Carrots');
+}
+
+await welcome()
+await askName();
+await president_question();
+await music_question();
+await language_question();
+await animal_question();
+await food_question();
+await winner();
